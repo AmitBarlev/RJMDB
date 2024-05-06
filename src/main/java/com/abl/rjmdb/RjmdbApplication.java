4f3fc081e2603347dd465488c42a5665a06c5ef0
AmitBarlev <Barlev.amitush@gmail.com>
2024-05-06 21:51:21 +0300

    Initialize spring project

---
+++ src/main/java/com/abl/rjmdb/RjmdbApplication.java
@@ -0,0 +1,13 @@
+package com.abl.rjmdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RjmdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RjmdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abl/rjmdb/RjmdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abl.rjmdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RjmdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
